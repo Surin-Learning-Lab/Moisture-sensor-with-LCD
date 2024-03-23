3b518e450c96fa5aa095971051ebc0036a384404
SurinLearninLab <155703944+SurinLearninLab@users.noreply.github.com>
2024-03-23 17:00:02 +0700

    Update moistureSensor.ino

--- moistureSensor.ino
+++ moistureSensor.ino
@@ -1,16 +1,13 @@
-// test code for relay moisture sensor with 16*2
-
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>  // Including the LiquidCrystal_I2C library
-
-// Removed unnecessary libraries and OLED related code
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Initializing the LCD screen with I2C address, 16 columns, and 2 rows
 
 int Moisture_signal = A0;  // Define the Analog pin# on the Arduino for the soil moisture sensor signal
 int relay = 6;
 int Moisture;
+unsigned long previousMillis = 0;  // Variable to store the last time moisture was checked
+const long interval = 3600000;     // Interval to check moisture (in milliseconds) - 1 hour
 
 void setup() {
   lcd.init();       // Initializing the LCD screen
@@ -21,30 +18,33 @@ void setup() {
   lcd.setCursor(0, 1);  // Setting the cursor position to column 0, row 1
   lcd.print(":)");  // Displaying the symbol ":)"
 
-  delay(50);  // Waiting for 2 seconds
+  delay(5000);  // Waiting for 2 seconds
 
   lcd.clear();  // Clearing the LCD screen
 
-  // Serial.begin(9600);  // Start the serial communication - commented out since it's not used
   pinMode(relay, OUTPUT); // Added pinMode for relay
 }
 
 void loop() {
-  Moisture = analogRead(Moisture_signal);
-  Serial.print("Soil Moisture Level: ");
-  Serial.println(Moisture);
+  unsigned long currentMillis = millis();  // Get the current time
 
-  lcd.setCursor(0, 0);         // Setting the cursor position to column 0, row 0
-  lcd.print("Moisture :");        // Displaying the text "Moisture :"
-  lcd.print(Moisture);            // Displaying the read value
-  lcd.print("    ");           // Displaying empty spaces to clear previous characters
-  
-  delay(1000);
-  if (Moisture < 1000 && Moisture > 500) {
-    digitalWrite(relay, HIGH);
-    delay(60000);
-  } else {
-    digitalWrite(relay, LOW);
+  if (currentMillis - previousMillis >= interval) {
+    // It's time to check the moisture
+    previousMillis = currentMillis;  // Save the last time moisture was checked
+
+    Moisture = analogRead(Moisture_signal);
+    Serial.print("Soil Moisture Level: ");
+    Serial.println(Moisture);
+
+    lcd.setCursor(0, 0);         // Setting the cursor position to column 0, row 0
+    lcd.print("Moisture :");        // Displaying the text "Moisture :"
+    lcd.print(Moisture);            // Displaying the read value
+    lcd.print("    ");           // Displaying empty spaces to clear previous characters
+
+    if (Moisture < 1000 && Moisture > 500) {
+      digitalWrite(relay, HIGH);
+      delay(120000);  // Delay for 2 minute
+      digitalWrite(relay, LOW);
+    }
   }
 }
-
